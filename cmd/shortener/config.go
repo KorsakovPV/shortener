012a42c9c4938630bb446d7bf3a3e44331407b8c
Pavel Korsakov <PKorsakov@datafort.ru>
2023-05-05 10:46:52 +0700

    iter4 added env

--- cmd/shortener/config.go
+++ cmd/shortener/config.go
@@ -2,6 +2,7 @@ package main
 
 import (
 	"flag"
+	"os"
 )
 
 type configuration struct {
@@ -15,4 +16,12 @@ func parseFlags() {
 	flag.StringVar(&config.flagRunAddr, "a", "localhost:8080", "address and port to run server")
 	flag.StringVar(&config.flagBaseURLAddr, "b", "http://localhost:8080", "address and port to run server")
 	flag.Parse()
+
+	if envRunAddr := os.Getenv("RUN_ADDR"); envRunAddr != "" {
+		config.flagRunAddr = envRunAddr
+	}
+
+	if envBaseURLAddr := os.Getenv("RUN_ADDR"); envBaseURLAddr != "" {
+		config.flagBaseURLAddr = envBaseURLAddr
+	}
 }
