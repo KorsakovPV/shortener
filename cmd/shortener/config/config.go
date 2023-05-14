a7580ed586c3c20c7017fb191a4789a12dba1f2e
Pavel Korsakov <PKorsakov@datafort.ru>
2023-05-14 13:57:49 +0700

    iter5 test fail

--- cmd/shortener/config/config.go
+++ cmd/shortener/config/config.go
@@ -10,26 +10,26 @@ type Сonfiguration struct {
 	FlagBaseURLAddr string
 }
 
-var Config = &Сonfiguration{}
+var config = &Сonfiguration{}
 
 //func init() {
 //	ParseFlags()
 //}
 
 func ParseFlags() {
-	flag.StringVar(&Config.FlagRunAddr, "a", "127.0.0.1:8080", "address and port to run server")
-	flag.StringVar(&Config.FlagBaseURLAddr, "b", "http://127.0.0.1:8080", "address and port to run server")
+	flag.StringVar(&config.FlagRunAddr, "a", "127.0.0.1:8080", "address and port to run server")
+	flag.StringVar(&config.FlagBaseURLAddr, "b", "http://127.0.0.1:8080", "address and port to run server")
 	flag.Parse()
 
 	if envRunAddr := os.Getenv("SERVER_ADDRESS"); envRunAddr != "" {
-		Config.FlagRunAddr = envRunAddr
+		config.FlagRunAddr = envRunAddr
 	}
 
 	if envBaseURLAddr := os.Getenv("BASE_URL"); envBaseURLAddr != "" {
-		Config.FlagBaseURLAddr = envBaseURLAddr
+		config.FlagBaseURLAddr = envBaseURLAddr
 	}
 }
 
 func GetConfig() *Сonfiguration {
-	return Config
+	return config
 }
