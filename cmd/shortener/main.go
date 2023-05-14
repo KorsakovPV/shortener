3d7b331f21e9a6efd3ec99e0286efb0517219a5a
Pavel Korsakov <PKorsakov@datafort.ru>
2023-05-14 13:51:03 +0700

    iter5 test fail

--- cmd/shortener/main.go
+++ cmd/shortener/main.go
@@ -9,13 +9,13 @@ import (
 
 func main() {
 
-	config.ParseFlags()
-	log.Printf("Shortener start on %s. Default base URL %s.", config.Config.FlagRunAddr, config.Config.FlagBaseURLAddr)
-	err := http.ListenAndServe(config.Config.FlagRunAddr, apiserver.Router())
+	//config.ParseFlags()
+	//log.Printf("Shortener start on %s. Default base URL %s.", config.Config.FlagRunAddr, config.Config.FlagBaseURLAddr)
+	//err := http.ListenAndServe(config.Config.FlagRunAddr, apiserver.Router())
 
-	//cfg := config.GetConfig()
-	//log.Printf("Shortener start on %s. Default base URL %s.", cfg.FlagRunAddr, cfg.FlagBaseURLAddr)
-	//err := http.ListenAndServe(cfg.FlagRunAddr, apiserver.Router())
+	cfg := config.GetConfig()
+	log.Printf("Shortener start on %s. Default base URL %s.", cfg.FlagRunAddr, cfg.FlagBaseURLAddr)
+	err := http.ListenAndServe(cfg.FlagRunAddr, apiserver.Router())
 	if err != nil {
 		log.Fatal(err)
 	}
