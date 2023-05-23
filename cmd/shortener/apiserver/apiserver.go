3a2be475c30e0de287dbaba9c24be3725ef53012
Pavel Korsakov <PKorsakov@datafort.ru>
2023-05-24 01:41:00 +0700

    added json

--- cmd/shortener/apiserver/apiserver.go
+++ cmd/shortener/apiserver/apiserver.go
@@ -1,12 +1,15 @@
 package apiserver
 
 import (
+	"encoding/json"
 	"fmt"
 	"github.com/KorsakovPV/shortener/cmd/shortener/config"
 	"github.com/KorsakovPV/shortener/cmd/shortener/logging"
 	"github.com/KorsakovPV/shortener/cmd/shortener/middleware"
 	"github.com/KorsakovPV/shortener/cmd/shortener/storage"
+	"github.com/KorsakovPV/shortener/internal/models"
 	"github.com/go-chi/chi/v5"
+	"go.uber.org/zap"
 	"io"
 	"net/http"
 )
@@ -35,6 +38,39 @@ func createShortURL() http.HandlerFunc {
 			rw.WriteHeader(http.StatusBadRequest)
 			return
 		}
+	}
+	return http.HandlerFunc(fn)
+}
+
+func createShortURLJson() http.HandlerFunc {
+	fn := func(rw http.ResponseWriter, r *http.Request) {
+		sugar := logging.GetSugarLogger()
+
+		sugar.Infoln("Create short url")
+
+		var req models.Request
+		dec := json.NewDecoder(r.Body)
+		if err := dec.Decode(&req); err != nil {
+			sugar.Debug("cannot decode request JSON body", zap.Error(err))
+			rw.WriteHeader(http.StatusInternalServerError)
+			return
+		}
+
+		id := storage.GetStorage().PutURL(req.Url)
+
+		resp := models.Response{
+			Result: fmt.Sprintf("%s/%s", config.GetConfig().FlagBaseURLAddr, id),
+		}
+
+		rw.Header().Set("Content-Type", "application/json")
+		rw.WriteHeader(http.StatusOK)
+
+		enc := json.NewEncoder(rw)
+		if err := enc.Encode(resp); err != nil {
+			sugar.Debug("error encoding response", zap.Error(err))
+			return
+		}
+
 	}
 	return http.HandlerFunc(fn)
 }
@@ -74,6 +110,7 @@ func Router() chi.Router {
 	r := chi.NewRouter()
 
 	r.Post("/", middleware.WithLogging(createShortURL()))
+	r.Post("/api/shorten", middleware.WithLogging(createShortURLJson()))
 	r.Get("/{id}", middleware.WithLogging(readShortURL()))
 	r.MethodNotAllowed(middleware.WithLogging(methodNotAllowed()))
 	return r
--- cmd/shortener/main_test.go
+++ cmd/shortener/main_test.go
@@ -1,6 +1,7 @@
 package main
 
 import (
+	"bytes"
 	"fmt"
 	"github.com/KorsakovPV/shortener/cmd/shortener/apiserver"
 	"github.com/stretchr/testify/assert"
@@ -12,9 +13,8 @@ import (
 	"testing"
 )
 
-func testRequest(t *testing.T, ts *httptest.Server, method,
-	path string) (*http.Response, string) {
-	req, err := http.NewRequest(method, ts.URL+path, nil)
+func testRequest(t *testing.T, ts *httptest.Server, method, path string, jsonBody io.Reader) (*http.Response, string) {
+	req, err := http.NewRequest(method, ts.URL+path, jsonBody)
 	require.NoError(t, err)
 
 	resp, err := ts.Client().Do(req)
@@ -35,6 +35,7 @@ func TestRouter(t *testing.T) {
 
 	testCases := []struct {
 		url          string
+		body         string
 		method       string
 		expectedCode int
 		expectedBody string
@@ -44,11 +45,14 @@ func TestRouter(t *testing.T) {
 		{url: `/`, method: http.MethodPut, expectedCode: http.StatusBadRequest, expectedBody: ""},
 		{url: `/`, method: http.MethodPatch, expectedCode: http.StatusBadRequest, expectedBody: ""},
 		{url: `/`, method: http.MethodDelete, expectedCode: http.StatusBadRequest, expectedBody: ""},
+		{url: `/api/shorten`, body: `{"url": "https://practicum.yandex.ru"}`, method: http.MethodPost, expectedCode: http.StatusOK, expectedBody: ""},
 	}
 
 	for _, tc := range testCases {
 		t.Run(tc.method, func(t *testing.T) {
-			resp, get := testRequest(t, ts, tc.method, tc.url)
+			jsonBody := []byte(tc.body)
+			bodyReader := bytes.NewReader(jsonBody)
+			resp, get := testRequest(t, ts, tc.method, tc.url, bodyReader)
 			defer resp.Body.Close()
 
 			assert.Equal(t, tc.expectedCode, resp.StatusCode, "Код ответа не совпадает с ожидаемым")
